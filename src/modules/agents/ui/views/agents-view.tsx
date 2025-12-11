879b9fbbd469a0b0b8a041206248117cea89c2d7
Parth Kumar Singh <parthksingh1@gmail.com>
2025-12-11 23:53:12 +0530

    Agent_Page

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -9,8 +9,10 @@ import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { DataPagination } from "../components/data-pagination";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { useRouter } from "next/navigation";
 
 export const AgentsView = () => {
+  const router = useRouter();
   const [filters, setFilters] = useAgentsFilters();
 
   const trpc = useTRPC();
@@ -20,7 +22,11 @@ export const AgentsView = () => {
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns}/>
+      <DataTable 
+      data={data.items} 
+      columns={columns}
+      onRowCLick={(row) => router.push(`/agents/${row.id}`)}
+      />
       <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
@@ -45,8 +51,6 @@ export const AgentsViewLoading = () => {
         />
     );
 };
-
-
 
 export const AgentsViewError = () => {
     return (
