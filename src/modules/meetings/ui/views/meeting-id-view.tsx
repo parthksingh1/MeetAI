38c506abfa2f28dc2f4a3879ed841b258e6af643
Parth Kumar Singh <parthksingh1@gmail.com>
2025-12-16 00:34:56 +0530

    Meeting_Variants

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -10,21 +10,25 @@ import { useRouter } from "next/navigation";
 import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
 import { useState } from "react";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props {
     meetingId: string;
 };
 
 export const MeetingIdView = ({ meetingId }: Props) => {
-        const trpc = useTRPC();
-        const router= useRouter();
-        const queryClient = useQueryClient();
+    const trpc = useTRPC();
+    const router = useRouter();
+    const queryClient = useQueryClient();
 
-        const [UpdateMeetingDialogOpen, setUpdateMeetingDialogOpen]= useState(false);
+    const [UpdateMeetingDialogOpen, setUpdateMeetingDialogOpen] = useState(false);
 
-        const {data} = useSuspenseQuery(
-            trpc.meetings.getOne.queryOptions({ id: meetingId}),
-        );
+    const { data } = useSuspenseQuery(
+        trpc.meetings.getOne.queryOptions({ id: meetingId }),
+    );
 
     const removeMeeting = useMutation(
         trpc.meetings.remove.mutationOptions({
@@ -40,35 +44,51 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     );
 
     const [RemoveConfirmation, confirmRemove] = useConfirm(
-            "Are you Sure?",
-            "The following action will remove this meeting"
+        "Are you Sure?",
+        "The following action will remove this meeting"
     );
 
     const handleRemoveMeeting = async () => {
         const ok = await confirmRemove();
 
-        if(!ok) return;
+        if (!ok) return;
 
-        await removeMeeting.mutateAsync({id: meetingId});
+        await removeMeeting.mutateAsync({ id: meetingId });
     }
+
+    const isActive = data.status === "active";
+    const isUpcoming = data.status === "upcoming";
+    const isCancelled = data.status === "cancelled";
+    const isCompleted = data.status === "completed";
+    const isProcessing = data.status === "processing";
 
     return (
         <>
-        <RemoveConfirmation />
-        <UpdateMeetingDialog
-          open={UpdateMeetingDialogOpen}
-          onOpenChange={setUpdateMeetingDialogOpen}
-          initialValues={data}
-         />
-        <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <MeetingIdViewHeader
-                meetingId={meetingId}
-                meetingName={data.name}
-                onEdit={() => setUpdateMeetingDialogOpen(true)}
-                onRemove={handleRemoveMeeting}
-             />
-            {JSON.stringify(data, null, 2)}
-        </div>
+            <RemoveConfirmation />
+            <UpdateMeetingDialog
+                open={UpdateMeetingDialogOpen}
+                onOpenChange={setUpdateMeetingDialogOpen}
+                initialValues={data}
+            />
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <MeetingIdViewHeader
+                    meetingId={meetingId}
+                    meetingName={data.name}
+                    onEdit={() => setUpdateMeetingDialogOpen(true)}
+                    onRemove={handleRemoveMeeting}
+                />
+                {isCancelled && <CancelledState />}
+                {isProcessing && <ProcessingState />}
+                {isCompleted && <div>Completed</div>}
+                {isActive && <ActiveState meetingId={meetingId} />}
+                {isUpcoming && (
+                  <UpcomingState 
+                    meetingId={meetingId}
+                    onCancelMeeting={() => {}}
+                    isCancelling={false}
+                />
+                )}
+            </div>
         </>
     );
 };
